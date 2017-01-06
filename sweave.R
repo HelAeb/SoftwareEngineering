7112349796202d2d0a7499a3ac217b01f24bae8a
Michèle Schoch <Michle Schoch>
2017-01-06 15:21:01 +0100

    first version sweave; problems running
    problems running sweave; no plot contained

---
+++ sweave.R
@@ -0,0 +1,53 @@
+# =============================================================================
+# University of St.Gallen
+# Course: HS16-7,610,1.00 Software Engineering for Economists
+# Authors: Helena Aebersold, Divna Nikolic, Michèle Schoch
+# Professor: Dr. Philipp Zahn
+# Date: 28.12.2016
+# =============================================================================
+
+# =============================================================================
+# Sweave File
+# - This File contains the code to compile the LateX .Rnw into a .tex which 
+#     we then can run normally to make a .pdf with all the outputs from R
+# =============================================================================
+
+# IMPORTANT NOTE:
+#   I work with Texmaker which saves the files as LateX files. When using other
+#   LateX editors make sure to save the one with code junks in .Rnw. Make changes
+#   only in that file, not in the normal .tex
+
+# read "EPFL-Sweave-powerdot.pdf" for more information on Sweave
+
+# The Sweave command is used in package "utils". Set your working directory right
+#   and run this code, which will return a .tex file
+
+# Set working directory
+if (Sys.info()[['login']] == 'Helena Aebersold') {
+  dir <- 'C:/Users/Helena Aebersold/Dropbox/HSG/Master/HS16/software_engineering_for_economists/SoftwareEngineering/'
+} else if (Sys.info()[['login']] == 'Divna') {
+  dir <- "C:/Users/Divna/Documents/Uni/Master/3. Semester/Kontextstudium/Software Engineering for Economists/Group Project/SoftwareEngineering/"
+} else if (Sys.info()[['login']] == 'Michèle') {
+  dir <- "C:/Users/Michèle/Documents/MAHS15/Software_Engineering_for_Economists/SoftwareEngineering"
+} else if (Sys.info()[['login']] == 'Feld_PhilippZahn') {
+  dir <- ""
+} else {
+  dir <- getwd()
+}
+setwd(dir)
+
+
+
+
+Sweave("tex_file.tex")
+
+
+# if you have problems running that .tex file in LateX, then make sure to adjust
+#   the roots:
+# http://tex.stackexchange.com/questions/153193/latex-error-sweave-sty-not-found
+
+
+
+
+
+
