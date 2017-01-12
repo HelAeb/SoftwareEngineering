28a56db9f4f5d6dc8eaadffef9fd6889993cca41
DivnaNikolic <divna_nikolic@hotmail.com>
2017-01-12 22:22:15 +0100

    set working directory
    path of directory of Divna & corrected title

--- sweave.R
+++ sweave.R
@@ -1,9 +1,9 @@
 # =============================================================================
 # University of St.Gallen
-# Course: HS16-9,272,1.00 Economics of Central Banking
-# Author: Helena Aebersold
-# Professor: Dr. Nicolas Cuche-Curti
-# Date: 20.10.2016
+# Course: HS16-7,610,1.00 Software Engineering for Economists
+# Authors: Helena Aebersold, Divna Nikolic, Michèle Schoch
+# Professor: Dr. Philipp Zahn
+# Date: 28.12.2016
 # =============================================================================
 
 # =============================================================================
@@ -12,12 +12,11 @@
 #     we then can run normally to make a .pdf with all the outputs from R
 # =============================================================================
 
+
 # IMPORTANT NOTE:
 #   I work with Texmaker which saves the files as LateX files. When using other
 #   LateX editors make sure to save the one with code junks in .Rnw. Make changes
 #   only in that file, not in the normal .tex
-
-# read "EPFL-Sweave-powerdot.pdf" for more information on Sweave
 
 # The Sweave command is used in package "utils". Set your working directory right
 #   and run this code, which will return a .tex file
@@ -25,8 +24,8 @@
 # Set working directory
 if (Sys.info()[['login']] == 'Helena Aebersold') {
   dir <- 'C:/Users/Helena Aebersold/Dropbox/HSG/Master/HS16/software_engineering_for_economists/SoftwareEngineering/'
-} else if (Sys.info()[['login']] == '') {
-  dir <- ""
+} else if (Sys.info()[['login']] == 'Divna') {
+  dir <- "C:/Users/Divna/Documents/Uni/Master/3. Semester/Kontextstudium/Software Engineering for Economists/Group Project/SoftwareEngineering/"
 } else if (Sys.info()[['login']] == '') {
   dir <- ""
 } else {
@@ -35,17 +34,9 @@ if (Sys.info()[['login']] == 'Helena Aebersold') {
 setwd(dir)
 
 
-
-
 Sweave("tex_file.tex")
-#
+
 
 # if you have problems running that .tex file in LateX, then make sure to adjust
 #   the roots:
 # http://tex.stackexchange.com/questions/153193/latex-error-sweave-sty-not-found
-
-
-
-
-
-
